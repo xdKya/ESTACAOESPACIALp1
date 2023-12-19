2373e742f5f51809c0d95a8537b6ca17e10392c9
xdKya <90745676+xdKya@users.noreply.github.com>
2023-12-19 15:47:21 -0300

    Add files via upload

---
+++ screens/homeScreen.js
@@ -0,0 +1,8 @@
+import react, { Component } from "react";
+import { Text } from "react-native";
+
+export default class HomeScreen extends Component {
+  render() {
+    return <Text>Tela Principal!</Text>;
+  }
+}
